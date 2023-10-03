d365bf5674f95353d14b3510a37b332a26c2c4d3
mauritsvanelteren <143629476+mauritsvanelteren@users.noreply.github.com>
2023-10-03 13:30:47 +0200

    Delete src directory

--- src/data-preparation/download_data.R
+++
@@ -1,8 +0,0 @@
-# Download dataset 1
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file("https://datasets.imdbws.com/title.ratings.tsv.gz", "imdb_ratings.gz")
-
-# Download dataset 2
-# dir.create('./data/dataset2')  # Uncomment if need to create directory with R
-download.file("https://datasets.imdbws.com/title.basics.tsv.gz", "imdb_genres.gz")
-
